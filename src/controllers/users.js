aeb27b17da5cfe31d3069942da032fc18580c370
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-05-10 19:55:11 -0700

    Fixing styles for registration modal

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,6 +4,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/logout', function(req, res, next) {
+  req.session.destroy();
+
+  res.redirect('/');
+});
+
 router.get('/login', function(req, res, next) {
   res.render('users/login');
 });
