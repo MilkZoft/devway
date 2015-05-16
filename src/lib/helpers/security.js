7d37b239657d22530d7e3bd231ba544720fe228a
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-05-16 01:26:13 -0700

    Removing validatingPost from security helper

--- src/lib/helpers/security.js
+++ src/lib/helpers/security.js
@@ -11,9 +11,5 @@ module.exports = {
 
   md5: function(str) {
     return crypto.createHash('md5').update(salt + str.toString()).digest('hex');
-  },
-
-  validatePost: function(securityToken) {
-
   }
 };
