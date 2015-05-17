1d0cef39e1ea967e21a8ed42c03de23d62580210
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-05-17 02:25:27 -0700

    Removing load & get methods from form helper & fixing hiddens

--- src/lib/helpers/form.js
+++ src/lib/helpers/form.js
@@ -4,44 +4,8 @@ var config = require('../config');
 var _ = require('lodash');
 var security = require('./security');
 var utils = require('./utils');
-var post = {};
 
 module.exports = {
-  load: function(body, securityToken) {
-    if (body[security.md5('securityToken')] === security.md5(securityToken)) {
-      post = body;
-    } else {
-      return false;
-    }
-  },
-
-  get: function(options) {
-    var input = options;
-    var filter = 'strong';
-    var value;
-
-    if (options instanceof Array && options.length === 2) {
-      input = options[0];
-      filter = options[1];
-    }
-
-    if (typeof post[security.md5(input)] !== 'undefined') {
-      value = post[security.md5(input)];
-
-      if (filter === 'escape') {
-        value = utils.escape(value);
-      } else if (filter === 'html') {
-        value = utils.removeHTML(value);
-      } else if (filter === 'strong') {
-        value = utils.escape(utils.removeHTML(value));
-      }
-
-      return value;
-    } else {
-      return false;
-    }
-  },
-
   createLabel: function(attrs, text) {
     var html = '<label ';
 
@@ -64,7 +28,7 @@ module.exports = {
       html += 'type="text" ';
     }
 
-    if (!hasClass && type !== ' hidden' && type !== 'checkbox' && type !== 'radio') {
+    if (!hasClass && type !== 'hidden' && type !== 'checkbox' && type !== 'radio') {
       html += 'class="input" ';
     }
 
